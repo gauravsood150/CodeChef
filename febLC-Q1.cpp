8bf46b8528229310575ef86c05c9275e7323881c
gauravsood150 <gsood0162@gmail.com>
2017-02-14 19:03:47 +0530

    Create febLC-Q1.cpp

---
+++ febLC-Q1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int T,N;
+  cin>>T;
+  while(T)
+  {
+      cin>>N;
+      int arr[N],sum=0,flag=0;
+      for(int i=0; i<N; i++) 
+      {
+        cin>>arr[i];
+        if(arr[i]==0)
+        {
+            flag=1;
+            sum+=1100;
+        }
+        if(arr[i] && flag)
+        {
+            sum+=100;
+        }
+      }
+      cout<<sum;
+      T--;
+    }
+}
